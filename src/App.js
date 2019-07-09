56e2fdef6587f72eae4d0225b0740c15bd23f399
Pedro Pinho <rihor1000@gmail.com>
2019-07-09 10:47:58 -0300

    Utilizando rotas e estilo global na base do app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
